9e5eafe2e43bdf52412f6e8635bdf9f037f3d42e
yosiarbilla <yosiarbl16@gmail.com>
2025-07-05 16:19:21 +0700

    db js fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
     try {
         await mongoose.connect(ENV.MONGO_URI);
         console.log("Connected to MongoDB");
-    }catch (error){
+    } catch (error){
         console.log("Error connecting to MongoDB:", error.message);
         process.exit(1);
     }
