7bc67b387393826fa23607986b7834b2433f1d4c
yosiarbilla <yosiarbl16@gmail.com>
2025-07-05 16:16:58 +0700

    startServer func fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,24 @@
 import express from "express"
 import { ENV } from "./config/env.js"; 
 import { connectDB } from "./config/db.js";
+
+
 const app = express();
 
-connectDB();
 
 app.get("/",(req,res)=>{
     res.send("Hello From Server");
 })
 
-app.listen(ENV.PORT, ()=>console.log("Server is up and running on PORT:", ENV.PORT));
+const startServer = async () => {
+    try {
+        await connectDB();
+        
+        app.listen(ENV.PORT, ()=>console.log("Server is up and running on PORT:", ENV.PORT));
+    } catch (error){
+        console.log("Error connecting to server:", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
